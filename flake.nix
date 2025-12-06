e22954365b104e76db064f43d04d4c5376e58477
agdral <ezequiel.escobar@proton.me>
2025-12-06 16:04:22 -0300

    init proyect

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "A Nix-flake-based Bun development environment";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = ["x86_64-linux"];
+    forEachSupportedSystem = f:
+      nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import nixpkgs {inherit system;};
+        });
+  in {
+    devShells = forEachSupportedSystem ({pkgs}: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          nodejs_22
+          pnpm
+        ];
+      };
+    });
+  };
+}
